e5997b6be78bdcb951d4a9c02d19026863bb0865
uraneko <BOOTHAlessandra@tutanota.com>
2025-12-15 09:01:58 +0100

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,62 @@
+use pheasant::http::{ErrorStatus, Protocol, Respond, status};
+use pheasant::services::{
+    Server, bad_request, parse, read_stream, req_buf, resp_write_stream, write_stream,
+};
+use std::io::BufReader;
+
+mod services;
+use services::{Socket, lookup};
+
+#[tokio::main]
+async fn main() -> Result<(), ErrorStatus> {
+    let mut root = String::new();
+
+    // let Ok(_) = std::io::stdin().read_line(&mut root) else {
+    //     return Ok(());
+    // };
+
+    let Ok(mut socket) = Socket::with_buf(root, Vec::new(), [127, 10, 10, 1], 6687) else {
+        return Ok(());
+    };
+
+    socket.init_message();
+    socket
+        .event_loop(async |this| {
+            // let mut buf: Vec<u8> = Vec::with_capacity(65536);
+            let mut resp = Respond::new(Protocol::Http11, status!(200));
+            while let Ok((mut stream, _)) = read_stream(&this.socket) {
+                resp.clear();
+                let mut reader = BufReader::new(&mut stream);
+                let Ok(req_buf) = req_buf(&mut reader) else {
+                    bad_request(&mut resp);
+                    resp_write_stream(&resp, &mut stream)?;
+                    continue;
+                };
+                let req = parse(req_buf);
+                let Ok(req) = req else {
+                    bad_request(&mut resp);
+                    resp_write_stream(&resp, &mut stream)?;
+                    continue;
+                };
+
+                // lookup should fetch whole service chains
+                let service = match lookup(&req, &mut resp) {
+                    Ok(s) => s,
+                    Err(_err) => {
+                        bad_request(&mut resp);
+                        resp_write_stream(&resp, &mut stream)?;
+                        continue;
+                    }
+                };
+                println!();
+                _ = this.service(req, &mut resp, service).await;
+                // println!("{}---", str::from_utf8(&resp.to_bytes()).unwrap());
+                resp_write_stream(&resp, &mut stream)?;
+            }
+
+            Ok(())
+        })
+        .await?;
+
+    Ok(())
+}
---
+++ src/services.rs
@@ -0,0 +1,125 @@
+use pheasant::http::{ErrorStatus, Respond, err_stt, header_value, request::Request};
+use pheasant::services::{
+    MessageBodyInfo, Range, Resource, Server, Service, bad_request, bind_socket, not_found,
+};
+use std::io::Read;
+
+pub struct Socket {
+    pub(crate) socket: std::net::TcpListener,
+    buffer: Vec<u8>,
+    root: String,
+}
+
+impl Socket {
+    pub fn with_buf(
+        root: String,
+        buffer: Vec<u8>,
+        addr: impl Into<std::net::Ipv4Addr>,
+        port: u16,
+    ) -> Result<Self, ErrorStatus> {
+        Ok(Self {
+            buffer,
+            socket: bind_socket(addr, port).map_err(|_| err_stt!(500))?,
+            root,
+        })
+    }
+}
+
+impl Server for Socket {
+    fn addr(&self) -> Result<std::net::SocketAddr, std::io::Error> {
+        self.socket.local_addr()
+    }
+
+    // falling back to the http default port when an error is encountered is nonsense
+    fn port(&self) -> u16 {
+        match self.socket.local_addr() {
+            Ok(addr) => addr.port(),
+            Err(_) => 80,
+        }
+    }
+}
+
+pub struct File(String);
+
+impl File {
+    fn new(req: &Request) -> Self {
+        Self(req.path()[2..].join("/"))
+    }
+}
+
+impl Resource<Socket> for File {
+    async fn get(
+        &self,
+        socket: &mut Socket,
+        req: Request,
+        resp: &mut Respond,
+    ) -> Result<(), ErrorStatus> {
+        let mut file = std::fs::File::open(&self.0).map_err(|_| err_stt!(500))?;
+
+        if let Some(range) = header_value(req.headers(), b"range") {
+            let Ok(range) = Range::new(range) else {
+                bad_request(resp);
+                return err_stt!(?400);
+            };
+
+            resp.headers_mut().extend(b"accept-ranges: bytes\n");
+            let n = range
+                .read(&mut file, resp.body_mut())
+                .map_err(|_| err_stt!(500))?;
+            MessageBodyInfo::with_len(n)
+                .guess_mime(resp.body_ref())
+                .dump_headers(resp.headers_mut());
+        } else {
+            let n = file
+                .read_to_end(resp.body_mut())
+                .map_err(|_| err_stt!(500))?;
+            MessageBodyInfo::with_len(n)
+                .guess_mime(resp.body_ref())
+                .dump_headers(resp.headers_mut());
+        }
+
+        Ok(())
+    }
+
+    async fn head(
+        &self,
+        socket: &mut Socket,
+        req: Request,
+        resp: &mut Respond,
+    ) -> Result<(), ErrorStatus> {
+        if let Some(range) = header_value(req.headers(), b"range") {
+            let Ok(_) = Range::new(range) else {
+                bad_request(resp);
+                return err_stt!(?400);
+            };
+
+            resp.headers_mut().extend(b"accept-ranges: bytes\n");
+        }
+
+        Ok(())
+    }
+
+    // fn post(&self, socket: &mut Socket, req: Request, buf: &mut Vec<u8>) {}
+}
+
+impl Service<Socket> for File {
+    async fn run(
+        &self,
+        socket: &mut Socket,
+        req: Request,
+        resp: &mut Respond,
+    ) -> Result<(), ErrorStatus> {
+        Resource::run(self, socket, req, resp).await
+    }
+}
+
+pub fn lookup(req: &Request, resp: &mut Respond) -> Result<File, ErrorStatus> {
+    let path = req.path();
+
+    if path.len() > 1 && path[..2] == ["", "file"] {
+        return Ok(File::new(&req));
+    }
+
+    not_found(resp);
+    err_stt!(?404)
+}
